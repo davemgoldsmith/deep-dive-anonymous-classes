18b5cb6b5e411672e57fc8043b31f42a03c29c63
Dave <davemgoldsmith@yahoo.com>
2017-07-10 08:57:02 -0600

    first commit

---
+++ swingx/src/edu/cnm/deepdive/swing/HelloWorld.java
@@ -0,0 +1,47 @@
+package edu.cnm.deepdive.swing;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class HelloWorld implements ActionListener {
+
+	private JFrame frame;
+	
+	private void createAndShowGui() {
+			frame = new JFrame("Hello World in Swing");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			JLabel label = new JLabel("Hello World");
+			frame.getContentPane().add(label);
+			frame.pack();
+			frame.setVisible(true);
+		}
+
+
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(()-> {new HelloWorld().createAndShowGui();});
+
+		
+				
+			
+				
+					
+		
+} //main
+
+
+} //HelloWorld Class
+
+
---
+++ swingx/src/edu/cnm/deepdive/swingx/swing.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.swing;
+
+/**
+ * @author davem
+ *
+ */
+public class swing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
